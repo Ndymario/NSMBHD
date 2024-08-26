7f99fdb1d8e2e897e8ff0f4b395a7a5bf3230151
Nolan Y? <ndymario@gmail.com>
2024-08-26 11:24:50 -0500

    Update markdown
    - Improve the [quote][/quote] regex
    - Add [url] parsing

--- webroot/plugins/webhook/init.php
+++ webroot/plugins/webhook/init.php
@@ -1,44 +1,113 @@
 <?php
-function replaceQuotes($string) {
-    // Define the pattern to match
-    $pattern = '/\[quote="([^"]+)" id="([^"]+)"\](.*?)\[\/quote\]/s';
-
-    while (preg_match($pattern, $string, $matches)) {
-        // Fix line breaks in nested quotes
-        $nestedContent = str_replace("\n", "\n\n", $matches[3]);
-        
-        // Append '>' in front of the text
-        $nestedContent = '> ' . str_replace("\n", "\n> ", $nestedContent);
-        
-        $replacementString = "[Quoting {$matches[1]}](https://nsmbhd.net/post/{$matches[2]}/)\n\n{$nestedContent}";
-        $string = str_replace($matches[0], $replacementString, $string);
+function replaceQuotes($input) {
+    // Helper function to add ">" to each new line and add an extra newline at the end
+    function formatQuoteContent($content) {
+        $lines = explode("\n", trim($content)); // Split the content into lines
+        $formattedLines = array_map(function($line) {
+            return "> " . trim($line); // Add "> " to each line and trim it
+        }, $lines);
+        return implode("\n", $formattedLines) . "\n"; // Rejoin the lines and add an extra newline at the end
     }
 
-    return $string;
+    // Pattern 1: Matches [quote="Ndymario"]Hi[/quote]
+    $pattern1 = '/\[quote="(.*?)"\](.*?)\[\/quote\]/is';
+    $replacement1 = function($matches) {
+        return "*Posted by " . $matches[1] . "*\n" . formatQuoteContent($matches[2]);
+    };
+    
+    // Pattern 2: Matches [quote="Ndymario" id=44]Hi[/quote]
+    $pattern2 = '/\[quote="(.*?)" id="?(\d+)"?\](.*?)\[\/quote\]/is';
+    if ($urlRewriting) {
+        $replacement2 = function($matches) {
+            return '[*Posted by ' . $matches[1] . '*](' . getServerURLNoSlash() . actionLink("post", $matches[2], "", "_") . ')' . "\n" . formatQuoteContent($matches[3]);
+        };
+    } else {
+        $replacement2 = function($matches) {
+            return '[*Posted by ' . $matches[1] . '*](' . getServerURL() . "?pid=" . $matches[2] . ')' . "\n" . formatQuoteContent($matches[3]);
+        };
+    }
+    
+    // Pattern 3: Matches [quote]Hi[/quote]
+    $pattern3 = '/\[quote\](.*?)\[\/quote\]/is';
+    $replacement3 = function($matches) {
+        return formatQuoteContent($matches[1]);
+    };
+
+    // Pattern 4: Matches [quote Ndymario id=44]Hi[/quote] (no equals sign for the name)
+    $pattern4 = '/\[quote\s+([^\]=\s]+(?:\s+id="?(\d+)"?)?)\](.*?)\[\/quote\]/is';
+    $replacement4 = function($matches) {
+        return "*Posted by " . $matches[1] . "*\n" . formatQuoteContent($matches[3]);
+    };
+
+    $output = preg_replace_callback($pattern2, $replacement2, $input); // First, handle quotes with IDs and author
+    $output = preg_replace_callback($pattern1, $replacement1, $output); // Then, handle quotes with author only
+    $output = preg_replace_callback($pattern4, $replacement4, $output); // Then, handle quotes without "="
+    $output = preg_replace_callback($pattern3, $replacement3, $output); // Finally, handle anonymous quotes
+
+    return $output;
 }
+
+
+function replaceURLs($input) {
+    // Pattern 1: Matches [url=<some url>]Hello world[/url]
+    $pattern1 = '/\[url=(.*?)\](.*?)\[\/url\]/i';
+    // Replacement: Convert to [Hello world](<some url>)
+    $replacement1 = '[$2]($1)';
+    
+    // Pattern 2: Matches [url]<some url>[/url]
+    $pattern2 = '/\[url\](.*?)\[\/url\]/i';
+    // Replacement: Convert to <some url>
+    $replacement2 = '$1';
+    
+    // First replace pattern 1
+    $output = preg_replace($pattern1, $replacement1, $input);
+    
+    // Then replace pattern 2
+    $output = preg_replace($pattern2, $replacement2, $output);
+    
+    return $output;
+}
+
 
 
 function formatPost($post){
 	$formatPost = $post;
 
+    // Code Block [source=py] | [/source] -> ```py | ```
+	$codePattern = "/\[source=(.*?)\]/";
+	$formatPost = preg_replace($codePattern, "```$1\n", $formatPost);
+	$formatPost = str_replace("[/source]", "\n```", $formatPost);
+
 	// Quote [quote="user" id="id"] | [/quote] -> [(Quoting user)](https://nsmbhd.net/post/id/)
 	$formatPost = replaceQuotes($formatPost);
+
+    // URL [url=<some url>] | [/url] -> [<the original message text>](<some url>)
+    // [url]<some url>[/url] = <some url>
+    $formatPost = replaceURLs($formatPost);
 
 	// Bold [b] | [/b] -> ** | **
 	$formatPost = str_replace("[b]", "**", $formatPost);
 	$formatPost = str_replace("[/b]", "**", $formatPost);
+    $formatPost = str_replace("[B]", "**", $formatPost);
+	$formatPost = str_replace("[/B]", "**", $formatPost);
 
 	// Italics [i] | [/i] -> * | *
 	$formatPost = str_replace("[i]", "*", $formatPost);
 	$formatPost = str_replace("[/i]", "*", $formatPost);
+    $formatPost = str_replace("[I]", "*", $formatPost);
+	$formatPost = str_replace("[/I]", "*", $formatPost);
 
 	// Underline [u] | [/u] -> __ | __
 	$formatPost = str_replace("[u]", "__", $formatPost);
 	$formatPost = str_replace("[/u]", "__", $formatPost);
+    $formatPost = str_replace("[U]", "__", $formatPost);
+	$formatPost = str_replace("[/U]", "__", $formatPost);
 
 	// Strikethrough [s] | [/s] -> ~~ | ~~
 	$formatPost = str_replace("[s]", "~~", $formatPost);
 	$formatPost = str_replace("[/s]", "~~", $formatPost);
+    $formatPost = str_replace("[S]", "~~", $formatPost);
+	$formatPost = str_replace("[/S]", "~~", $formatPost);
 
 	// Code [code] | [/code] -> ` | `
 	$formatPost = str_replace("[code]", "`", $formatPost);
@@ -48,17 +117,12 @@ function formatPost($post){
 	$formatPost = str_replace("[spoiler]", "||", $formatPost);
 	$formatPost = str_replace("[/spoiler]", "||", $formatPost);
 
-	// Code Block [source=py] | [/source] -> ```py | ```
-	//	TODO - Make it so the specified code source type is preserved
-	$codePattern = "/\[source=(.*?)\]/";
-	$formatPost = preg_replace($codePattern, "```$1\n", $formatPost);
-	$formatPost = str_replace("[/source]", "\n```", $formatPost);
-
 	return $formatPost;
 }
 
 function postWebhook($title, $description, $url, $color, $authorName, $authorID, $webhookUrl, $webhookUsername, $webhookAvatar){
     $timestamp = date("c", strtotime("now"));
+    $serverURL = getServerURL();
     $json_data = json_encode([
         
         // Username
@@ -68,9 +132,6 @@ function postWebhook($title, $description, $url, $color, $authorName, $authorID,
         // Uncomment to replace image set in webhook
         "avatar_url" => $webhookAvatar,
     
-        // Text-to-speech
-        "tts" => false,
-    
         // File upload
         // "file" => "",
     
@@ -79,26 +140,22 @@ function postWebhook($title, $description, $url, $color, $authorName, $authorID,
             [
                 // Embed Title
                 "title" => $title,
-    
-                // Embed Type
-                "type" => "rich",
-    
+
                 // Embed Description
                 "description" => formatPost($description),
-    
+
                 // URL of title link
                 "url" => $url,
-    
+
                 // Timestamp of embed must be formatted as ISO8601
                 "timestamp" => $timestamp,
-    
+
                 // Embed left border color in HEX
                 "color" => $color,
-    
+
                 // Author
                 "author" => [
-                    "name" => $authorName,
-                    "avatar_url" => "https://nsmbhd.net/data/avatars/$authorID/"
+                    "name" => $authorName
                 ]
             ]
         ]
@@ -115,4 +172,7 @@ function postWebhook($title, $description, $url, $color, $authorName, $authorID,
 
     $response = curl_exec($ch);
     curl_close($ch);
+    // if ($response != "") {
+    //     kill(__("Something went wrong with the webhook. $response   $url"));
+    // }
 }
